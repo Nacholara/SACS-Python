09a9ebdd76c0336508baae615672f5314bf58644
unknown <SG0946321@H9480305561996.Global.ad.sabre.com>
2016-02-01 14:42:13 +0100

    Dependencies installation script and library

--- com/sabre/api/sacs/rest/Rest.py
+++ com/sabre/api/sacs/rest/Rest.py
@@ -5,8 +5,10 @@ Created on Nov 27, 2015
 '''
 from com.sabre.api.sacs.rest.activities.LeadPriceCalendarActivity import LeadPriceCalendarActivity
 from com.sabre.api.sacs.workflow.Workflow import Workflow
+from com.sabre.api.sacs.rest.activities.MCT import MCT
 
 workflow = Workflow(LeadPriceCalendarActivity())
+# workflow = Workflow(MCT())
 sharedContext = workflow.runWorkflow()
 print("---------------------- RESULTS --------------------------")
 print(sharedContext.leadPriceCalendarResult.text)
